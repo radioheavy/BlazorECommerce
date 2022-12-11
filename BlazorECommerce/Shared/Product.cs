90ff4d2263f37af3b2f42b55c765aa08fd025452
İsmail Oktay Dak <oktaydak34@gmail.com>
2022-12-11 04:59:42 +0300

    Add project files.

---
+++ BlazorECommerce/Shared/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorECommerce.Shared
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+        /*Özelliğin ilk değeri string.Empty olarak ayarlanmıştır, yani ilk değeri boş bir dize olacaktır.*/
+
+        public string Description { get; set; } = string.Empty;
+
+        public string ImageURL { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+
+
+
+    }
+}
